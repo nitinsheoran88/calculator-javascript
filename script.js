a279fa1527d186351da1e8096fac2e43ee8492b4
nitinsheoran88 <nitinsheoran47@gmail.com>
2025-02-27 22:39:57 +0530

    project updated

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('js file is working........')
+
+let result = document.querySelector('.box1 input')
+
+function addInput(Value){
+    result.value += Value
+}
+function allClear(){
+    result.value = ''
+}
+function showResult(){   
+    try{
+        result.value = eval(result.value)
+    }catch(error){
+        result.value = 'Error'
+    }
+}
